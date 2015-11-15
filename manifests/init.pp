0e53f9abb795772851a154a68eef30c5254beab1
Pablo Ros <pablo.ros@gmail.com>
2015-11-15 12:49:04 +0100

    Añadir time zone y ntp class para hora del sistema

--- manifests/init.pp
+++ manifests/init.pp
@@ -58,4 +58,14 @@ class mysite16
 
   # Create files
   include mysite16::createfiles
+
+  # Ensure Time Zone and Region.
+  class { 'timezone':
+    timezone => 'Europe/Madrid',
+  }
+
+  #NTP
+  class { '::ntp':
+    server => [ '1.es.pool.ntp.org', '2.europe.pool.ntp.org', '3.europe.pool.ntp.org' ],
+  }
 }

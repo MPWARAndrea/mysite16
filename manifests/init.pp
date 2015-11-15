6ddd1ab6a6a902ef9a3a022c7c20ce64efd20b71
Pablo Ros <pablo.ros@gmail.com>
2015-11-15 22:08:17 +0100

    Firewall: compatible con Centos 7 y Centos 6.5

--- manifests/init.pp
+++ manifests/init.pp
@@ -68,4 +68,36 @@ class mysite16
   class { '::ntp':
     server => [ '1.es.pool.ntp.org', '2.europe.pool.ntp.org', '3.europe.pool.ntp.org' ],
   }
+
+  # Ip Tables.
+  if $operatingsystemrelease == '7.0.1406'
+  {
+    # firewalld - Centos 7
+    firewalld_rich_rule { 'Accept HTTP':
+      ensure  => present,
+      zone    => 'public',
+      service => 'http',
+      action  => 'accept',
+    }
+  }
+  else
+  {
+    package { 'iptables':
+      ensure => present,
+      before => File['/etc/sysconfig/iptables'],
+    }
+    file { '/etc/sysconfig/iptables':
+      ensure  => file,
+      owner   => "root",
+      group   => "root",
+      mode    => 600,
+      replace => true,
+      source  => "puppet:///modules/iptables/iptables.txt",
+    }
+    service { 'iptables':
+      ensure     => running,
+      enable     => true,
+      subscribe  => File['/etc/sysconfig/iptables'],
+    }
+  }
 }
